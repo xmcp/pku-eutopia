fb5a71cfebdedfd10c3302d659e66363f494e969
xmcp <xmcp@pku.edu.cn>
2022-09-17 18:52:12 +0800

    maybe the session expired check is buggy?

--- src/api/common.js
+++ src/api/common.js
@@ -1,13 +1,13 @@
 function alert_session_expired() {
-    try {
-        let last_ts = parseInt(localStorage.getItem('EUTOPIA_LAST_SESSION_EXPIRED_TS') || '0', 10);
-        if(last_ts && (Date.now() - last_ts) < 30*1000) // re-logined within 30s, must be some bug, ignore
-            return;
-
-        localStorage.setItem('EUTOPIA_LAST_SESSION_EXPIRED_TS', '' + (+Date.now()));
-    } catch(e) {
-        console.log('cannot detect last session expired time', e);
-    }
+    // try {
+    //     let last_ts = parseInt(localStorage.getItem('EUTOPIA_LAST_SESSION_EXPIRED_TS') || '0', 10);
+    //     if(last_ts && (Date.now() - last_ts) < 30*1000) // re-logined within 30s, must be some bug, ignore
+    //         return;
+    //
+    //     localStorage.setItem('EUTOPIA_LAST_SESSION_EXPIRED_TS', '' + (+Date.now()));
+    // } catch(e) {
+    //     console.log('cannot detect last session expired time', e);
+    // }
 
     window.location.href = '/site/login/cas-login';
 }
